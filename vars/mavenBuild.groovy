ec8df3406b6b7240db29a8e45f38b18ecd92311f
Vanshika4222 <67817404+Vanshika4222@users.noreply.github.com>
2021-04-14 12:04:16 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -3,31 +3,21 @@ def call(String repoUrl) {
    
        node {
            stage("Tools initialization") {
-				steps {
                    sh "mvn --version"
                    sh "java -version"
-               }
            }
            stage("Checkout Code") {
-				steps {
                    git branch: 'master',
                        url: "${repoUrl}"
-               }
            }
-           stage("Cleaning workspace") {
-				steps {
+	   stage("Cleaning workspace") {
                    sh "mvn clean"
-               }
            }
            stage("Running Testcase") {
-				steps {
                    sh "mvn test"
-               }
            }
            stage("Packing Application") {
-				steps {
                    sh "mvn package -DskipTests"
-               }
            }
        }
    }
