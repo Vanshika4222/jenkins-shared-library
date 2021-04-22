3c02931811504070900fbad9555211c7f4f3eaba
Vanshika4222 <67817404+Vanshika4222@users.noreply.github.com>
2021-04-22 13:47:34 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,7 +1,18 @@
+apiVersion: v1
+kind: Pod
+metadata:
+  name: maven-pod
+spec:
+  containers:  # list of containers that you want present for your build, you can define a default container in the Jenkinsfile
+    - name: maven
+      image: maven:3.5.4-jdk-8-slim
+      command: ["tail", "-f", "/dev/null"]  # this or any command that is bascially a noop is required, this is so that you don't overwrite the entrypoint of the base container
+      imagePullPolicy: Always # use cache or pull image for agent
+
 def call(String script) {
       echo "Hello Vanshika welcome to MavenBuild shared library"
    
-       node {
+       node ('maven-pod') {
            stage("Tools initialization") {
                    sh "mvn --version"
                    sh "java -version"
